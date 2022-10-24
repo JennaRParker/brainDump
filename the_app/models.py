2cfd500bdbc7e20ac04413845e46e87f7f12e6dd
JENNA PARKER <github-actions[bot]@users.noreply.github.com>
2022-10-24 12:17:13 -0400

    creates post model

--- the_app/models.py
+++ the_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    caption = models.TextField(max_length=1000)
+    date = models.DateTimeField(auto_now_add=True)
+    #comment
+    #likes
+    #dislikes
+    #category many to many field
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return(self.caption)
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'post_id': self.id})
