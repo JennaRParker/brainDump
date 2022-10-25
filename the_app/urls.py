45e8e0e2dd0169c857bee9197162926e4968318f
JENNA PARKER <github-actions[bot]@users.noreply.github.com>
2022-10-25 13:46:14 -0400

    alters initial page to be login

--- the_app/urls.py
+++ the_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.signup, name='signup'),
     path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('add/', views.PostCreate.as_view(), name='post_create'),
